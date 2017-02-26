b6195cbe2975565f1bdbcb90b95a2d650ece2cc6
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-02-25 23:59:31 -0600

    Asignaciones

--- GoProgrammingLanguage/command_line_args.go
+++ GoProgrammingLanguage/command_line_args.go
@@ -2,7 +2,12 @@ package main
 import ("fmt"
     "os")
 
+// la función main es obligatoria
+
 func main() {
     fmt.Println("Este es un programa que lee los argumentos de consola: \n")
     fmt.Println(os.Args)
+
+    // Args es un slice
+    cml_arguments := os.Args[1]
 }
