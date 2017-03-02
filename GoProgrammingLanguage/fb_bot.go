144125d7c17f4f84ac1dd4a72a86597108f360f6
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-03-02 00:02:52 -0600

    Estructura completa para recibir mensaje

--- GoProgrammingLanguage/fb_bot.go
+++ GoProgrammingLanguage/fb_bot.go
@@ -38,5 +38,19 @@ func Verify(w http.ResponseWriter, r *http.Request) {
 type MessengerPayload struct{
     Entry []struct{
         Time    uint64 `json:"time,omitempty"`
+        Messaging []struct {
+            Sender struct {
+                Id string `json:"id"`
+            } `json:"sender,omitempty"`
+            Recipient struct {
+                Id string `json:id`
+            } `json:"recipient,omitempty"`
+            Timestamp uint64 `json:"timestamp,omitempty"`
+            Message *struct {
+                Mid string `json:"mid,omitempty"`
+                Seq uint64 `json:"seq,omitempty"`
+                Text string `json:"text"`
+            } `json:message,omitempty`
+        } `json:"messaging"`
     }
 }
