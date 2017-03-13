10e504e8aded4f252ede8df42fce70c63719112c
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-03-12 23:19:48 -0600

    primer prueba string.Join

--- GoProgrammingLanguage/echo_index_fmt.go
+++ GoProgrammingLanguage/echo_index_fmt.go
@@ -9,9 +9,10 @@ import (
 func main() {
 	var sep string
 
-    // en cada iteración declara index y toma el argumento
+	// en cada iteración declara index y toma el argumento
 	for index, arg := range os.Args[1:] {
 		fmt.Printf(sep + " %d " + arg, index)
 		sep = "\n"
 	}
+
 }
