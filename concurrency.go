a4d393eeb4b2d3134e2aaa98260ab302cb8bf654
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-06-22 23:56:57 -0500

    Printing time waiting

--- concurrency.go
+++ concurrency.go
@@ -8,15 +8,15 @@ import (
 
 func f(n int) {
 	for i := 0; i < 10; i++ {
-		fmt.Println(n, ":", i)
 		amt := time.Duration(rand.Intn(250))
+		fmt.Println(n, ":", i , ":", amt)
 		time.Sleep(time.Millisecond * amt)
 	}
 }
 
 func main() {
 	for i := 0; i < 10; i++ {
-		go f(i)
+		go f(i) // ?
 	}
 	var input string
 	fmt.Scanln(&input)
