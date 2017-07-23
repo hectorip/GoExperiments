01e576e8aafc5bfbf183f7ec0842de7e8d115577
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-07-22 23:54:35 -0500

    binarios - explicaciones básicas

--- GoProgrammingLanguage/helloworld.go
+++ GoProgrammingLanguage/helloworld.go
@@ -1,9 +1,15 @@
-// El paquete principal siempre es main
+// El paquete principal siempre es main, y al existir
+// indica que este programa/archivo no es una biblioteca
+// sino un programa ejecutable por sí mismo
 package main
 
+// No se pueden importar paquetes que no se ocupen
 import "fmt"
 
-// No se pueden importar paquetes que no se ocupen
+// La función main indica el punto de entrada de un programa,
+// aquí se define lo que el programa hace.
 func main() {
-	fmt.Println("Hello, 世界!") // Sekai - mundo en japonés
+    // go es unicode, por lo que puede soportar sin porblema carácteres
+    // de todos los kenguajes
+	fmt.Println("Hello, 世界!") // Sekai - mundo en japonés 
 }
