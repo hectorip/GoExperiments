4ec7872a64a7b43789ffdff09f5c24ef70bf7ec3
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-07-27 23:59:49 -0500

    explicaciones

--- GoProgrammingLanguage/helloworld.go
+++ GoProgrammingLanguage/helloworld.go
@@ -1,15 +1,16 @@
-// El paquete principal siempre es main, y al existir
+// El paquete principal se llama 'main', y al existir
 // indica que este programa/archivo no es una biblioteca
 // sino un programa ejecutable por sí mismo
 package main
 
-// No se pueden importar paquetes que no se ocupen
+// Paquetes importados, para usarse por el programa actual
 import "fmt"
+// No se pueden importar paquetes que no se ocupen
 
 // La función main indica el punto de entrada de un programa,
 // aquí se define lo que el programa hace.
 func main() {
-    // go es unicode, por lo que puede soportar sin porblema carácteres
-    // de todos los kenguajes
+    // go es unicode, por lo que puede soportar carácteres
+    // de todos los lenguajes
 	fmt.Println("Hello, 世界!") // Sekai - mundo en japonés 
 }
