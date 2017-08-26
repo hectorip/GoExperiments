a619b723f1362f6864b9c322c95a08738a8d9229
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-08-25 23:35:09 -0500

    Contador de líneas por achivo

--- GoProgrammingLanguage/capitulo_1/contador_lineas/contador_archivo.go
+++ GoProgrammingLanguage/capitulo_1/contador_lineas/contador_archivo.go
@@ -1,6 +1,34 @@
 // Contador de líneas repetidas sólo de archivos indicados
+// Se lee todo el archivo, se cara en memoria y después se procesa
+// no se usa "streaming"
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
+func main() {
+	fmt.Println("Contando líneas en archivo")
+	files := os.Args[1:] // Cada archivo pasado al programa como argumento
+	counts := make(map[string]int)
+	for _, file := range files {
+
+		data, err := ioutil.ReadFile(file)
+		// fmt.Println("Contando líneas del archivo: " + file)
+		if err != nil {
+			fmt.Printf("Error leyendo archivo %s", file)
+			continue
+		}
+		for _, line := range strings.Split(string(data), "\n") { // seprando por líneas
+			counts[line]++
+		}
+	}
+	for line, count := range counts {
+		if count > 1 {
+			fmt.Printf("%d\t%s\n", count, line)
+		}
+	}
 }
