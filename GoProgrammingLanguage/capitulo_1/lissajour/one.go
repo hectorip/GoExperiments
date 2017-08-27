d62cdb5e575b066f256f0eac767d5e8eaa2d1201
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-08-27 00:00:07 -0500

    Beginning with lissajous

--- GoProgrammingLanguage/capitulo_1/lissajour/one.go
+++ GoProgrammingLanguage/capitulo_1/lissajour/one.go
@@ -1,6 +1,5 @@
 // Crea un Gif con una figura de Lissajour
 // (curvas extrañas)
-
 package main
 
 import (
@@ -8,7 +7,16 @@ import (
 	"image"
 	"image/color"
 	"image/gif"
+	"io"
+	"math"
 	"math/rand"
+	"os"
+)
+
+var palette = []color.Color{color.Red, color.Blue} // Declaramos la paleta de Colores que usaremos
+const (
+	redIndex  = 0
+	blueIndex = 1
 )
 
 func main() {
