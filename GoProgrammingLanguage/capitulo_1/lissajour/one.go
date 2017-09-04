82b774b3b4e4718f0572bcb150f84125b6f2320e
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-09-03 23:59:50 -0500

    Experimenting with colors

--- GoProgrammingLanguage/capitulo_1/lissajour/one.go
+++ GoProgrammingLanguage/capitulo_1/lissajour/one.go
@@ -11,29 +11,33 @@ import (
 	"math"
 	"math/rand"
 	"os"
+	// "sys"
+	"strconv"
 )
 
 // Declaramos la paleta de Colores que usaremos
 // El primero lo usaremos como fondo y el segundo como color de líneas
-var palette = []color.Color{color.White, color.RGBA{255, 10, 200, 1}}
+var palette = []color.Color{color.White, color.RGBA{255, 10, 200, 1}, color.RGBA{0, 200, 50, 1}}
 
 const (
 	whiteIndex = 0 // Colores que se usraán para las imágenes
 	blackIndex = 1 // Color de línea
+	greenIndex = 2 // Color de línea
 )
 
 func main() {
 	lissajous(os.Stdout)
 }
 func lissajous(out io.Writer) {
+	args = os.Args[1:]
 	const (
-		cycles  = 10
+		cycles  = strconv.Atoi(args[0])
 		res     = 0.001
 		size    = 100
 		nframes = 64
 		delay   = 8
 	)
-	freq := rand.Float64() * 10.0
+	freq := rand.Float64() * strcnv.Atoi(10.0)
 	anim := gif.GIF{LoopCount: nframes} // Creando un GIF
 	phase := 0.0
 	for i := 0; i < nframes; i++ { // Creando cada cuadro de la animación
@@ -43,7 +47,8 @@ func lissajous(out io.Writer) {
 			x := math.Sin(t)
 			y := math.Sin(t*freq + phase)
 			// fmt.Println(size + int(x*size+0.5))
-			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), blackIndex)
+			index := uint8(rand.Intn(2) + 1)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), index)
 		}
 		phase += 0.1
 		anim.Delay = append(anim.Delay, delay)
