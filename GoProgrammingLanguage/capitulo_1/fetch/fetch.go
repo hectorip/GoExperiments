502b4eb529b17a3bc2aca9a96deae2963875f88d
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-10-09 00:31:06 -0500

    fetchgin data with fetch

--- GoProgrammingLanguage/capitulo_1/fetch/fetch.go
+++ GoProgrammingLanguage/capitulo_1/fetch/fetch.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"fmt"
-	"ioutils"
+	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 func main() {
-	for i, url := range sys.Args[1:] {
+	for i, url := range os.Args[1:] {
 		resp, err := http.Get(url)
-
+		if err != nil {
+			fmt.Printf("Error retrieving: %s, Error: %s", url, resp)
+		}
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Printf("Error reading response from %s, Error: %s", url, err)
+		}
+		fmt.Printf("%d -> %s", i+1, body)
 	}
 }
