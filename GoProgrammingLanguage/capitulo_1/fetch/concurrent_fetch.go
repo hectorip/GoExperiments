bd8115bcc65314e390e68af554d7487eea027a0b
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-10-09 01:04:57 -0500

    concurrent fetch fixed

--- GoProgrammingLanguage/capitulo_1/fetch/concurrent_fetch.go
+++ GoProgrammingLanguage/capitulo_1/fetch/concurrent_fetch.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-	"fmt",
-	"io/ioutil",
-	"net/http",
-	"time",
-	"os"
+	"fmt"
 	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
 )
 
-func main(){
+func main() {
 	start := time.Now()
 	ch := make(chan string)
-	for _, url := os.Args[1:] {
-		go timed_fetch(url, recv)
+	for _, url := range os.Args[1:] {
+		go timed_fetch(url, ch)
 	}
 	for range os.Args[1:] {
 		fmt.Println(<-ch) // Recibiendo del canal, se bloquea hasta que recibe algo
@@ -21,7 +21,7 @@ func main(){
 	fmt.Printf("%.2fs tiempo total", time.Since(start).Seconds())
 }
 
-func timed_fetch(url string, recv chan){
+func timed_fetch(url string, ch chan<- string) {
 	start := time.Now()
 	resp, err := http.Get(url)
 	if err != nil {
