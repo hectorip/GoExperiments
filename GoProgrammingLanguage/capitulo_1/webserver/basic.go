0593eac74da8b0b072fc5716bcde289d20f4ba34
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-10-11 23:59:15 -0500

    webserver try

---
+++ GoProgrammingLanguage/capitulo_1/webserver/basic.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.Handle("/", basicHandler)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func basicHandler(writer http.ResponseWriter, request *http.Request) {
+	fmt.Fprint(writer, "Hello Writer Go!")
+	return
+}
