1ea9e2174f5c5ed789067fcb2df0b47f3cfe5dcc
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2018-10-02 23:35:46 -0500

    variables

--- basicos/variables.go
+++ basicos/variables.go
@@ -1,1 +1,13 @@
-// Declaraciónd e variables.
+// Declaración e inicialización de variables
+// Según el libro "The Go Programming Language", hay cuatro tipos de declaraciones:
+
+// forma corta, sólo para usarse dentro de funciones,
+// infiere el tipo por el valor asginado
+name := ""
+
+var nombre string // declaración explícita de tipo, usa el valor inicial para el tipo de dato
+var nombre = ""   // util para cuando se quieren declarar varias variables
+
+// lo mismo que la anterior, pero es útil para cuando se asigna
+// un tipo de dato diferente (cast)
+var nombre string = ""
